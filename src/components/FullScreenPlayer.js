8787d9a7d211133abbf10dedbb5dcb492ba7fd6c
zaursuleymnv <szaur018@mail.ru>
2024-01-06 00:54:53 +0100

    Full screen arrangement

--- src/components/FullScreenPlayer.js
+++ src/components/FullScreenPlayer.js
@@ -10,8 +10,26 @@ export default function FullScreenPlayer({toggle, state, controls, volumeIcon}) 
 
   return (
     <div className='relative h-full'>
-        <div className='absolute inset-0 object-cover bg-center bg-cover blur-md opacity-25' style={{backgroundImage: `url(${current.image})`}}></div>
-      <div className='w-full absolute bottom-4 flex flex-col items-center px-8'>
+        <div className='absolute inset-0 object-cover bg-center bg-cover blur-md opacity-25' style={{backgroundImage: `url(${current.image})`}}/>
+        <div className='absolute top-10 left-8 text-white flex items-center gap-x-4 opacity-60'>
+            <Icon name="logo" size={34}/>
+            <div className='text-xs'>
+                <p style={{fontSize: 11}}>PLAYING FROM PLAYLIST</p>
+                <h6 className='font-semibold mt-0.5 uppercase'>{current.title}</h6>
+            </div>
+        </div>
+        <div className='absolute left-8 bottom-36 flex items-center gap-x-5'>
+            <img src={current.image} className='w-24 h-24 object-cover' alt="" />
+            <div className='self-end'>
+                <h3 className='text-3xl font-bold opacity-70'>{current.title}</h3>
+                <p className='text-sm font-medium opacity-50'>{current.description}</p>
+            </div>
+        </div>
+      <div
+        onClick={e => {
+            e.stopPropagation()
+        }} 
+        className='w-full absolute bottom-4 flex flex-col items-center px-8'>
         <div className='w-full flex items-center gap-x-2 mb-1.5'>
             <div className='text-[0.688rem] text-white text-opacity-70'>
             {secondsToTime(state?.time)}
@@ -27,7 +45,6 @@ export default function FullScreenPlayer({toggle, state, controls, volumeIcon}) 
                 {secondsToTime(state?.duration)}
             </div>
         </div>
-            <div>sol</div>
             <div className='flex items-center gap-x-5'>
                 <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
                     <Icon name="shuffle" size={24}/>
@@ -67,8 +84,8 @@ export default function FullScreenPlayer({toggle, state, controls, volumeIcon}) 
                 />
                 </div>
                 <button
-                onClick={toggle} 
-                className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'
+                    onClick={toggle} 
+                    className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'
                 >
                 <Icon name="fullScreenOff" size={24}/>
                 </button>
---
+++ src/data/songs.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        id: 1,
+        title: 'Intouchables - Soundtracks OST',
+        description: 'Original Soundtrack',
+        artist: 'Zaur Suleymanov',
+        image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
+        type: 'album',
+        src: 'https://freesound.org/data/previews/612/612095_5674468-lq.mp3' 
+      },
+      {
+        id: 2,
+        title: 'Peaceful Piano',
+        description: 'Relax and indulge with beautiful piano pieces',
+        artist: 'Manuş Baba',
+        image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
+        type: 'album',
+        src: 'https://freesound.org/data/previews/612/612092_7037-lq.mp3'
+      },
+      {
+        id: 3,
+        title: 'Hang Massive',
+        description: 'Artist',
+        artist: 'Cem Adrian',
+        image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
+        type: 'artist',
+        src: 'https://freesound.org/data/previews/612/612087_1648170-lq.mp3'
+      },
+      {
+        id: 4,
+        title: 'Intouchables - Soundtracks OST',
+        description: 'Original Soundtrack',
+        artist: 'Mark Eliyahu',
+        image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
+        type: 'podcast'
+      },
+      {
+        id: 5,
+        title: 'Intouchables - Soundtracks OST',
+        description: 'Original Soundtrack',
+        artist: 'Manuş Baba',
+        image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
+        type: 'album',
+        src: 'https://freesound.org/data/previews/612/612085_28867-lq.mp3'
+      },
+]
